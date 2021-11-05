0ff772aec9552d330808d21a6192b0401ee62954
Maori <maorif.dev@gmail.com>
2021-11-04 21:13:21 -0300

    problema no calculo da média

--- M1S02/Atualiza secções/script.js
+++ M1S02/Atualiza secções/script.js
@@ -2,3 +2,34 @@
 function Participar(){
     document.getElementsByClassName("form")[0].style.display = "block";
 }
+
+let inputs = document.getElementsByClassName("data");
+let data = {number: 0, meanAge: 0, satisfied: 0};
+let dataShown = document.getElementsByClassName("number");
+
+function RefreshData(inputs){
+    data.number += 1;
+
+    if(data.meanAge == 0){
+        data.meanAge = Number(inputs[2].value);
+    }
+    else{
+        data.meanAge = ((data.meanAge + Number(inputs[2].value))/2 ).toFixed(2);
+    }
+    
+    if (inputs[3].value == 'sim'){
+        data.satisfied += 1;
+    }
+}
+
+function Enviar(){
+    RefreshData(inputs);
+
+    dataShown[0].innerHTML = data.number;
+    dataShown[1].innerHTML = data.meanAge;
+    dataShown[2].innerHTML = data.satisfied;
+
+    for (i=0; i<inputs.length; i++){
+        //inputs[i].value = "";
+    }
+}
